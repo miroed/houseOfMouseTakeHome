57b968fc4673af33c836f2d934b4ea1e8344d7dd
Miroed <edmond.mr@gmail.com>
2021-06-03 10:06:42 -0400

    feat: scroll to current element center

--- src/index.js
+++ src/index.js
@@ -2,65 +2,84 @@
   fetch("https://cd-static.bamgrid.com/dp-117731241344/home.json")
     .then((response) => response.json())
     .then(({data}) => {
-			// Get all categories
+      // Get all categories
       const categories = data.StandardCollection.containers.reduce(
         (acc, { set }, index) => {
-					let size = 0;
-					let refId = null;
-					let setId = null;
+          let size = 0;
+          let refId = null;
+          let setId = null;
 
-					if (set.meta && set.meta.page_size) size = set.meta.page_size;
-					if (set.refId) refId = set.refId;
-					if (set.setId) setId = set.setId;
+          if (set.meta && set.meta.page_size) size = set.meta.page_size;
+          if (set.refId) refId = set.refId;
+          if (set.setId) setId = set.setId;
 
-            acc[index] = {
-              title: set.text.title.full.set.default.content,
-              id: setId,
-							refId,
-              size,
-            };
+          acc[index] = {
+            title: set.text.title.full.set.default.content,
+            id: setId,
+            refId,
+            size,
+          };
           return acc;
         },
         {}
       );
 
-			// get all content in matrix format
-			const content = data.StandardCollection.containers.map(({ set }) => {
+      // get all content in matrix format
+      const content = data.StandardCollection.containers.map(({ set }) => {
         if (!set.items) return [];
         return set.items;
       });
 
       const mouse = new Mouse(categories, content);
-			mouse.populateContent($('#app'));
-			
-			$('body').on('keydown', function(event) {
-				const keyMap  = {
-					38: 'up',
-					40: 'down',
-					37: 'left',
-					39: 'right'
-				}
+      mouse.populateContent($("#app"));
 
-				if (Object.keys(keyMap).includes(`${event.which}`)) {
-					mouse.moveFocus(keyMap[`${event.which}`]);
-				}
-			});
+      // set how much of the category block do we need to see
+      // 	before we act on it
+      const options = {
+        threshold: 0.6,
+      };
+      // bind the mouse object to the observer so we can use
+      // it's methods/properties with in the observer callback
+      const categoryMouseObserver = categoryObserver.bind(mouse);
 
-			// set how much of the category block do we need to see
-			// 	before we act on it
-			const options = {
-				threshold: 0.6
-			}
-			// bind the mouse object to the observer so we can use
-			// it's methods/properties with in the observer callback
-			const categoryMouseObserver = categoryObserver.bind(mouse);
+      const observer = new IntersectionObserver(categoryMouseObserver, options);
 
-			const observer = new IntersectionObserver(categoryMouseObserver, options);
+      // loop through the categories to observe
+      [...$(".category")].forEach((category) => {
+        observer.observe(category, observer);
+      });
 
-			// loop through the categories to observe
-			[...$('.category')].forEach(category => {
-				observer.observe(category, observer);
-			})
+      $("body").on("keydown", function (event) {
+        event.preventDefault();
+        const keyMap = {
+          38: "up",
+          40: "down",
+          37: "left",
+          39: "right",
+        };
+
+        if (Object.keys(keyMap).includes(`${event.which}`)) {
+          mouse.moveFocus(keyMap[`${event.which}`]);
+
+          if (
+            keyMap[`${event.which}`] === "down" ||
+            keyMap[`${event.which}`] === "up"
+          ) {
+            const focusedTile = document.querySelector(
+              ".category__item--focused"
+            );
+
+            window.scrollTo({
+              left: 0,
+              top:
+                focusedTile.offsetTop +
+                focusedTile.offsetHeight -
+                window.innerHeight / 2,
+              behavior: "smooth",
+            });
+          }
+        }
+      });
     })
     .catch((err) => console.error(err));
 })();
