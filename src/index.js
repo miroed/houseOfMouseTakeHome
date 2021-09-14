4ecfbc146fb51127f624163b44ef41780057c696
Miroed <edmond.mr@gmail.com>
2021-09-14 13:41:51 -0400

    feat: updated to remove jquery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 (function init() {
   fetch("https://cd-static.bamgrid.com/dp-117731241344/home.json")
     .then((response) => response.json())
-    .then(({data}) => {
+    .then(({ data }) => {
       // Get all categories
       const categories = data.StandardCollection.containers.reduce(
         (acc, { set }, index) => {
@@ -31,7 +31,7 @@
       });
 
       const mouse = new Mouse(categories, content);
-      mouse.populateContent($("#app"));
+      mouse.populateContent(document.getElementById("app"));
 
       // set how much of the category block do we need to see
       // 	before we act on it
@@ -39,47 +39,49 @@
         threshold: 0.6,
       };
       // bind the mouse object to the observer so we can use
-      // it's methods/properties with in the observer callback
+      // its methods/properties with in the observer callback
       const categoryMouseObserver = categoryObserver.bind(mouse);
 
       const observer = new IntersectionObserver(categoryMouseObserver, options);
 
       // loop through the categories to observe
-      [...$(".category")].forEach((category) => {
+      [...document.querySelectorAll(".category")].forEach((category) => {
         observer.observe(category, observer);
       });
 
-      $("body").on("keydown", function (event) {
-        const keyMap = {
-          38: "up",
-          40: "down",
-          37: "left",
-          39: "right",
-        };
+      document
+        .querySelector("body")
+        .addEventListener("keydown", function (event) {
+          const keyMap = {
+            ArrowUp: "up",
+            ArrowDown: "down",
+            ArrowLeft: "left",
+            ArrowRight: "right",
+          };
 
-        if (Object.keys(keyMap).includes(`${event.which}`)) {
-          event.preventDefault();
-          mouse.moveFocus(keyMap[`${event.which}`]);
+          if (Object.keys(keyMap).includes(`${event.code}`)) {
+            event.preventDefault();
+            mouse.moveFocus(keyMap[`${event.code}`]);
 
-          if (
-            keyMap[`${event.which}`] === "down" ||
-            keyMap[`${event.which}`] === "up"
-          ) {
-            const focusedTile = document.querySelector(
-              ".category__item--focused"
-            );
+            if (
+              keyMap[`${event.code}`] === "down" ||
+              keyMap[`${event.code}`] === "up"
+            ) {
+              const focusedTile = document.querySelector(
+                ".category__item--focused"
+              );
 
-            window.scrollTo({
-              left: 0,
-              top:
-                focusedTile.offsetTop +
-                focusedTile.offsetHeight -
-                window.innerHeight / 2,
-              behavior: "smooth",
-            });
+              window.scrollTo({
+                left: 0,
+                top:
+                  focusedTile.offsetTop +
+                  focusedTile.offsetHeight -
+                  window.innerHeight / 2,
+                behavior: "smooth",
+              });
+            }
           }
-        }
-      });
+        });
     })
     .catch((err) => console.error(err));
 })();
